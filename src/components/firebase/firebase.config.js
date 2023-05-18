eed9c9acd6baf7c395edbd13433ad973813fb596
BelayetHossen <ntarget50@gmail.com>
2023-05-18 21:09:40 +0600

    Footer section

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -1,3 +1,6 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -5,14 +8,31 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_apiKey,
-  authDomain:import.meta.env.VITE_authDomain,
-  projectId:import.meta.env.VITE_projectId,
-  storageBucket:import.meta.env.VITE_storageBucket,
-  messagingSenderId:import.meta.env.VITE_messagingSenderId,
-  appId:import.meta.env.VITE_appId
+  apiKey: "AIzaSyClDerOCOQG5zsf7t0l4-GMwHmSNlexaGg",
+  authDomain: "assignment-11-a242a.firebaseapp.com",
+  projectId: "assignment-11-a242a",
+  storageBucket: "assignment-11-a242a.appspot.com",
+  messagingSenderId: "644511879701",
+  appId: "1:644511879701:web:5fef0568af6dcdc413341c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+
+
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:import.meta.env.VITE_apiKey,
+//   authDomain:import.meta.env.VITE_authDomain,
+//   projectId:import.meta.env.VITE_projectId,
+//   storageBucket:import.meta.env.VITE_storageBucket,
+//   messagingSenderId:import.meta.env.VITE_messagingSenderId,
+//   appId:import.meta.env.VITE_appId
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 export default app;
--- src/components/layouts/App.jsx
+++ src/components/layouts/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
   return (
-    <div className="bg-slate-800 text-slate-200">
+    <div className="bg-slate-300 text-slate-200">
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
